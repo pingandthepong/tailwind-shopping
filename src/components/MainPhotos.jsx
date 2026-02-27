d4a54eb90f6c6a436afd472aaa75b2ca740ae726
pingandthepong <pingandthepong@gmail.com>
2026-02-27 17:07:36 +0900

    lighthouse Performance 개선(이미지 해상도 리사이징 및 포맷 변경, 초기공간 확보)

--- src/components/MainPhotos.jsx
+++ src/components/MainPhotos.jsx
@@ -1,37 +1,33 @@
 import React from "react";
-import Images01 from "../../public/shop_detail_01.webp";
-import Images02 from "../../public/shop_detail_02.jpg";
-import Images03 from "../../public/shop_detail_03.jpg";
+import Images01 from "../../public/shop_detail_01_600.webp";
+import Images02 from "../../public/shop_detail_02_600.webp";
+import Images03 from "../../public/shop_detail_03_600.webp";
 
 const MainPhotos = () => {
+  const imgWrapBase = "basis-1/3 aspect-square overflow-hidden";
+  const pictureBase = "w-full h-full object cover";
+  const imgBase =
+    "w-full h-full object cover transition-scale duration-300 ease-in-out scale-100 hover:scale-110";
+
   return (
     <div className="flex gap-4 mt-2 mb-8 ">
-      <div className="basis-1/3 overflow-hidden">
-        <img
-          src={Images01}
-          alt="image 1"
-          className="transition-all duration-300 ease-in-out scale-100 hover:scale-110"
-          loading="lazy"
-          decoding="async"
-        />
+      <div className={imgWrapBase}>
+        <picture className={pictureBase}>
+          <source srcset={Images01} type="image/webp" />
+          <img src={Images01} alt="image 1" className={imgBase} />
+        </picture>
       </div>
-      <div className="basis-1/3 overflow-hidden">
-        <img
-          src={Images03}
-          alt="image 2"
-          className="transition-all duration-300 ease-in-out scale-100 hover:scale-110"
-          loading="lazy"
-          decoding="async"
-        />
+      <div className={imgWrapBase}>
+        <picture className={pictureBase}>
+          <source srcset={Images03} type="image/webp" />
+          <img src={Images03} alt="image 2" className={imgBase} />
+        </picture>
       </div>
-      <div className="basis-1/3 overflow-hidden">
-        <img
-          src={Images02}
-          alt="image 3"
-          className="transition-all duration-300 ease-in-out scale-100 hover:scale-110"
-          loading="lazy"
-          decoding="async"
-        />
+      <div className={imgWrapBase}>
+        <picture className={pictureBase}>
+          <source srcset={Images02} type="image/webp" />
+          <img src={Images02} alt="image 3" className={imgBase} />
+        </picture>
       </div>
     </div>
   );
